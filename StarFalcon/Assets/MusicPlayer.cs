af144553401d2650593c480c7f0857beefe91e78
PC-90\pc-90 <wojciech.stachowiak@playsoft.fr>
2019-10-28 15:01:53 +0100

    1st commit

---
+++ StarFalcon/Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene",5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
