10f748b7045254df6898436be8056d69b482d384
Wojciech Stachowiak <stachowiakw@gmail.com>
2019-10-29 21:49:46 +0100

    Lecture 88

--- StarFalcon/Assets/ShipController.cs
+++ StarFalcon/Assets/ShipController.cs
@@ -6,6 +6,10 @@ using UnityStandardAssets.CrossPlatformInput;
 public class ShipController : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 20f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 15f;
+    [SerializeField] float maxRangeLR = 6f;
+    [SerializeField] float maxRangeUD = 4f;
     void Start()
     {
         
@@ -14,7 +18,27 @@ public class ShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        setYpos();
+        setXpos();
+    }
+
+    void setXpos()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = Mathf.Clamp(transform.localPosition.x + xOffsetThisFrame, -maxRangeLR, maxRangeLR);
+
+        print(xOffsetThisFrame);
+        transform.localPosition = new Vector3(rawNewXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+
+    void setYpos()
+    {
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffsetThisFrame = yThrow * ySpeed * Time.deltaTime;
+        float rawNewYPos = Mathf.Clamp(transform.localPosition.y + yOffsetThisFrame, -maxRangeUD, maxRangeUD);
+
+        print(yOffsetThisFrame);
+        transform.localPosition = new Vector3(transform.localPosition.x, rawNewYPos, transform.localPosition.z);
     }
 }
