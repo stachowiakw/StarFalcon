24a4ee6d9b9aa4939df580003091a3cc53fa4b23
PC-90\pc-90 <wojciech.stachowiak@playsoft.fr>
2019-10-30 11:03:07 +0100

    Lecture 89

--- StarFalcon/Assets/ShipController.cs
+++ StarFalcon/Assets/ShipController.cs
@@ -8,8 +8,14 @@ public class ShipController : MonoBehaviour
     // Start is called before the first frame update
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 20f;
     [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 15f;
-    [SerializeField] float maxRangeLR = 6f;
+    [SerializeField] float maxRangeLR = 5f;
     [SerializeField] float maxRangeUD = 4f;
+    [SerializeField] float rotYawFactor = 7f;
+    [SerializeField] float rotPitchPosFactor = 9f;
+    [SerializeField] float rotPitchThrowFactor = 40f;
+    [SerializeField] float rotRollThrowFactor = -40f;
+
+    float xThrow, yThrow;
     void Start()
     {
         
@@ -20,11 +26,12 @@ public class ShipController : MonoBehaviour
     {
         setYpos();
         setXpos();
+        ProcessRotation();
     }
 
     void setXpos()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
         float rawNewXPos = Mathf.Clamp(transform.localPosition.x + xOffsetThisFrame, -maxRangeLR, maxRangeLR);
 
@@ -34,11 +41,25 @@ public class ShipController : MonoBehaviour
 
     void setYpos()
     {
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffsetThisFrame = yThrow * ySpeed * Time.deltaTime;
         float rawNewYPos = Mathf.Clamp(transform.localPosition.y + yOffsetThisFrame, -maxRangeUD, maxRangeUD);
 
         print(yOffsetThisFrame);
         transform.localPosition = new Vector3(transform.localPosition.x, rawNewYPos, transform.localPosition.z);
+    }
+
+    void ProcessRotation()
+    {
+        float pitchFromPosition = -transform.localPosition.y * rotPitchPosFactor;
+        float pitchFromTrust = yThrow * rotPitchThrowFactor;
+        float yawFromPosition = transform.localPosition.x * rotYawFactor;
+        float rollFromTrust = xThrow * rotRollThrowFactor;
+
+        
+        float pitch = pitchFromPosition - pitchFromTrust;
+        float yaw = yawFromPosition;
+        float roll = rollFromTrust;
+        transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
     }
 }
